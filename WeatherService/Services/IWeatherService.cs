a2f7ea5f3a48e750780441d2319bb6b86d76123f
yarik111475 <yarik111475@mail.ru>
2018-06-11 20:50:19 +0400

    Service created

---
+++ WeatherService/Services/IWeatherService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+using WeatherService.Models;
+
+namespace WeatherService.Services {
+    [ServiceContract]
+    public interface IWeatherService {
+        [OperationContract]
+        void UploadData();
+        [OperationContract]
+        CityModel[] GetCities();
+        [OperationContract]
+        WeatherModel GetWeatherById(int id);
+    }
+}
---
+++ WeatherService/Services/WeatherService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+using WeatherService.Models;
+
+namespace WeatherService.Services {
+    public class WeatherService : IWeatherService {
+        public CityModel[] GetCities() {
+            throw new NotImplementedException();
+        }
+
+        public WeatherModel GetWeatherById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public void UploadData() {
+            throw new NotImplementedException();
+        }
+    }
+}
