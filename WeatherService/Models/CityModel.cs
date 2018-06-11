8e2f510f00307ae3cf26ac3d3cd987c22655b124
yarik111475 <yarik111475@mail.ru>
2018-06-11 20:44:11 +0400

    Models created

---
+++ WeatherService/Models/CityModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace WeatherService.Models {
+    [DataContract]
+    public class CityModel {
+        [DataMember]
+        public string ID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string URL { get; set; }
+
+        public CityModel() { }
+        public CityModel(string id, string name) {
+            ID = id;
+            Name = name;
+        }
+    }
+}
---
+++ WeatherService/Models/WeatherModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace WeatherService.Models {
+    [DataContract]
+    public class WeatherModel {
+        [DataMember]
+        public string ID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string URL { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+        [DataMember]
+        public string Temperature { get; set; }
+
+        public WeatherModel() { }
+        public WeatherModel(string id, string date, string name) {
+            ID = id;
+            DateTime tempDate = DateTime.Now;
+            Date = DateTime.TryParse(date, out tempDate) ? tempDate : tempDate;
+            Name = name;
+        }
+    }
+}
